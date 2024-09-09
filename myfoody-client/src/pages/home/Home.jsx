21d3a1346fb328476fe0b684d58b752f9fc7aba1
Madhushan13 <96137093+Madhushan13@users.noreply.github.com>
2024-09-10 03:33:11 +0530

    completed Home page

--- myfoody-client/src/pages/home/Home.jsx
+++ myfoody-client/src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import Banner from "../../components/Banner";
 import Categories from "./Categories";
 import SpecialDishes from "./SpecialDishes";
 import Testimonials from "./Testimonials";
+import OurServices from "./OurServices";
 
 export const Home = () => {
   return (
@@ -11,6 +12,7 @@ export const Home = () => {
       <Categories />
       <SpecialDishes />
       <Testimonials />
+      <OurServices />
     </div>
   );
 };
